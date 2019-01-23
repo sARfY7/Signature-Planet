149f342b5a652e9efa6f2eb03dd6f56f4b47416a
Sarfaraz Ahmed <imsarfy@gmail.com>
2019-01-23 15:22:18 +0530

    Slider Added

---
+++ js/main.js
@@ -0,0 +1,59 @@
+$(function() {
+  var sideNavOut = false;
+  $(".destination").on("click", function() {
+    if (sideNavOut) {
+      anime({
+        targets: ".side-nav",
+        translateX: "-100%",
+        easing: "easeOutQuint"
+      });
+      $(".slider").css("filter", "blur(0)");
+      anime({
+        targets: ".slider",
+        translateX: "0",
+        scale: "1",
+        easing: "easeOutQuint"
+      });
+      $(".arrow-1").css("transform", "rotate(45deg)");
+      $(".arrow-2").css("transform", "rotate(-45deg)");
+      $(".scroll").css("opacity", "1");
+      sideNavOut = false;
+    } else {
+      anime({
+        targets: ".side-nav",
+        translateX: ["-100%", "0"],
+        easing: "easeOutQuint"
+      });
+      anime({
+        targets: ".slider",
+        translateX: "30%",
+        filter: "blur(2px)",
+        scale: "1.01",
+        easing: "easeOutQuint"
+      });
+      $(".arrow-1").css("transform", "rotate(135deg)");
+      $(".arrow-2").css("transform", "rotate(-135deg)");
+      $(".scroll").css("opacity", "0");
+      sideNavOut = true;
+    }
+  });
+
+  $(".destination").on("mouseover", function() {
+    if (sideNavOut) {
+      $(".arrow-1").css("transform", "rotate(135deg)");
+      $(".arrow-2").css("transform", "rotate(-135deg)");
+    } else {
+      $(".arrow-1").css("transform", "rotate(45deg)");
+      $(".arrow-2").css("transform", "rotate(-45deg)");
+    }
+  });
+  $(".destination").on("mouseout", function() {
+    if (sideNavOut) {
+      $(".arrow-1").css("transform", "rotate(135deg)");
+      $(".arrow-2").css("transform", "rotate(-135deg)");
+    } else {
+      $(".arrow-1").css("transform", "rotate(0)");
+      $(".arrow-2").css("transform", "rotate(0)");
+    }
+  });
+});
