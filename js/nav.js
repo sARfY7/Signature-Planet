b3417b31966200cbd441d3863e7acdfbf2c1012d
Sarfaraz Ahmed <imsarfy@gmail.com>
2019-03-14 01:28:59 +0530

    Major Design Change V2

--- js/nav.js
+++ js/nav.js
@@ -1,29 +1,12 @@
-console.clear();
+const $window = $(window);
+const vHeight = window.innerHeight;
 
-var app = (function() {
-  var body = void 0;
-  var menu = void 0;
-  var menuItems = void 0;
-
-  var init = function init() {
-    body = document.querySelector("body");
-    menu = document.querySelector(".menu-icon");
-    menuItems = document.querySelectorAll(".nav__list-item");
-
-    applyListeners();
-  };
-
-  var applyListeners = function applyListeners() {
-    menu.addEventListener("click", function() {
-      return toggleClass(body, "nav-active");
-    });
-  };
-
-  var toggleClass = function toggleClass(element, stringClass) {
-    if (element.classList.contains(stringClass))
-      element.classList.remove(stringClass);
-    else element.classList.add(stringClass);
-  };
-
-  init();
-})();
+$window.on("scroll", function() {
+  // console.log(vHeight);
+  if ($window.scrollTop() > vHeight) {
+    $(".nav").css("background-color", "#fff");
+  }
+  if ($window.scrollTop() < vHeight) {
+    $(".nav").css("background-color", "transparent");
+  }
+});
